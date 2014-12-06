80d6de097c0b85d354b57bcf1e2c3d46f08ec728
Johannes Löthberg <johannes@kyriasis.com>
2014-12-06 17:56:36 +0100

    Relicense under GPL2 only
    Also add missing license headers to missing files.

--- src/yawa.c
+++ src/yawa.c
@@ -1,16 +1,16 @@
 /*
+ * yawa - Yet Another Wallpaper Application
  * Copyright (C) 2003, Hyriand <hyriand@thegraveyard.org>
- * Copyright (C) 2014, Johannes Löthberg <johannes@kyriasis.com>
+ * Copyright (C) 2014, The YAWA Developers
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * This file is part of yawa.  yawa is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, version 2.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
